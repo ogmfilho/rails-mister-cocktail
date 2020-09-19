f040352d3d537093186f571e0bcf2a1157f4fbc0
Otoniel Miranda <ogmfilho@gmail.com>
2020-09-18 23:01:53 -0300

    Doses controllers and views added; Cocktail show view updated with doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,34 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to @cocktail, notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
